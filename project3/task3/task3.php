a2b6c79ecf7d2249b9325b93275adcc3d77955ff
Pustoy277353 <polikarpov587@gmail.com>
2023-11-21 13:06:49 +0300

    практическая 2, задания 2,3

---
+++ project3/task3/task3.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div>
+        <form method="post">
+            <p>Имя</p>
+            <input type="text" name="name" required>
+            <p>Телефон1</p>
+            <input type="tel" name="phone1" required>
+            <p>Телефон2(если есть)</p>
+            <input type="tel" name="phone2"><br>
+            <button type="submit" name="button">Отправить</button>
+        </form>
+
+        <?php
+
+        if (isset($_POST['button'])) {
+            $file = 'task3.txt';
+            $contact = file_get_contents($file);
+            $contact = 'Имя:' . $_POST['name'] . "\n" . 'Телефон1:' . $_POST['phone1'] . "\n" . 'Телефон2:' . $_POST['phone2'] . "\n\n";
+            file_put_contents($file, $contact, FILE_APPEND);
+        }
+        ?>
+    </div>
+</body>
+
+</html>
